c2331729aeff7ff7d47255bb241c252b224492c4
Aspen Zachman <azachman66@gmail.com>
2026-03-06 19:14:39 -0500

    fixing again

--- public/js/comic_settings.js
+++ public/js/comic_settings.js
@@ -15,19 +15,19 @@ const maxpg = 1; //the current number of pages your comic has in total. this DOE
 //YOU MUST UPDATE THIS NUMBER EVERY TIME YOU ADD A NEW PAGE or else it wont display the most recent page
 
 // COMIC PAGE SETTINGS
-const folder = "img/comics"; //directory of the folder where you keep all the comics
+const folder = "../img/comics"; //directory of the folder where you keep all the comics
 const image = "pg"; //what you'll name all your comic pages
 const imgPart = "_" //special character(s) you put after the page number to subdivide pages into multiple image files (ie pg2_1, pg2_2, etc)
 const ext = "png"; //file extension of your comic pages
 
 //THUMBNAIL SETTINGS
-const thumbFolder = "img/thumbs" //directory of the folder where you keep all the thumbnail images for the comics, in case you want the archive page to use thumbnails.
+const thumbFolder = "../img/thumbs" //directory of the folder where you keep all the thumbnail images for the comics, in case you want the archive page to use thumbnails.
 const thumbExt = "png" //file extension of thumbnails
 const thumbDefault = "default" //name of the default thumbnail that displays when no thumbnail is set, located in the directory you set thumbFolder to.
 
 //NAVIGATION SETTINGS
 const navText = ["First","Previous","Next","Last"]; //alt text for your nav images, or just the text that shows up if you're not using images
-const navFolder = "img/comicnav"; //directory where nav images are stored
+const navFolder = "../img/comicnav"; //directory where nav images are stored
 const navExt = "png" //file extension of nav images
 const navScrollTo = "#showComic"; //id of the div you want the page to automatically scroll to when you click to the next comic. will turn off if you delete text between quotation marks
 
