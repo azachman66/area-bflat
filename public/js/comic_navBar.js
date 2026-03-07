05239bc176112ea53e6d5ec21d1b3c283a201139
Aspen Zachman <azachman66@gmail.com>
2026-03-06 19:54:26 -0500

    adjusting css

---
+++ public/js/comic_navBar.js
@@ -0,0 +1,11 @@
+document.querySelector(".writeNavBar").innerHTML = `
+
+<div id="nav">
+<ul>
+            <li><a href="../index.html">HOME</a></li>
+            <li><a href="../pages/archive.html">ARCHIVE</a></li>
+            <li><a href="../pages/about.html">ABOUT</a></li>
+        <li><a href="../pages/characters.html">CHARACTERS</a></li>
+</ul>
+        </div>
+        `;
