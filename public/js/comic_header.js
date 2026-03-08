a9c7a12cb28147428aaa739e3505141647fb6f34
Aspen Zachman <azachman66@gmail.com>
2026-03-08 00:25:15 -0500

    playing around with the css for the main comic page

--- public/js/comic_header.js
+++ public/js/comic_header.js
@@ -1,7 +1,7 @@
 //the header of the site would be handled in this javascript file, so you don't have to copypaste the whole thing onto every page.
 //at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeHeader"
 document.querySelector(".writeHeader").innerHTML = `
-    <header align="left">
+    <header align="center">
         <a href="../pages/comic.html"><img src="../img/logo.png" alt="" /></a> 
     </header>
 `;
--- public/js/comic_navBar.js
+++ public/js/comic_navBar.js
@@ -1,11 +1,11 @@
 document.querySelector(".writeNavBar").innerHTML = `
-
-<div id="nav">
-<ul>
-            <li><a href="../index.html">HOME</a></li>
-            <li><a href="../pages/archive.html">ARCHIVE</a></li>
-            <li><a href="../pages/about.html">ABOUT</a></li>
-        <li><a href="../pages/characters.html">CHARACTERS</a></li>
-</ul>
-        </div>
+<div id="navbar">
+<div class="d-grid gap-2 d-md-block">
+  <button><a href="../index.html">HOME</a></button>
+  <button><a href="../pages/about.html">ABOUT</a></button>
+  <button><a href="../pages/characters.html">CHARACTERS</a></button>
+  <button><a href="../pages/archive.html">ARCHIVE</a></button>
+  <button><a href="../pages/credits.html">CREDITS</a></button>
+</div>
+</div>
         `;
