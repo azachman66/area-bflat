6c1dd6a94f96ac0134a490d8ce1e994f69ac5210
Aspen Zachman <azachman66@gmail.com>
2026-03-13 15:03:02 -0400

    adding images to all the character pages

--- public/js/comic_footer.js
+++ public/js/comic_footer.js
@@ -3,6 +3,6 @@
 document.querySelector(".writeFooter").innerHTML = `
     <footer align="center">
         <p>Here's a footer where you can put copyright information and stuff. There's also a link back to Rarebit. It's ultimately your choice whether to delete it or not but it'd be cool of you to keep it there and spread the word...</p> 
-        <p><strong>Powered by:</strong> <a href="https://rarebit.neocities.org"><img src="img/rarebitlogo_small.png" height = "30" /></a></p>
+        <p><strong>Powered by:</strong> <a href="https://rarebit.neocities.org"><img src="../img/rarebitlogo_small.png" height = "30" /></a></p>
     </footer>
 `;
