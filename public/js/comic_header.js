7b1d505c67fc9cf2f3845901803f2024c233a4d8
Aspen Zachman <azachman66@gmail.com>
2026-03-22 15:40:38 -0400

    .

--- public/js/comic_header.js
+++ public/js/comic_header.js
@@ -1,7 +1,7 @@
 //the header of the site would be handled in this javascript file, so you don't have to copypaste the whole thing onto every page.
 //at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeHeader"
 document.querySelector(".writeHeader").innerHTML = `
-    <header align="center">
+    <header align="center" style="margin: 0 auto;">
         <a href="../pages/comic.html"><img src="../img/AB-FLAT_logo.png" alt="AREA B-FLAT" style="width:50%; height: auto; margin: 0 auto;" /></a> 
     </header>
 `;
