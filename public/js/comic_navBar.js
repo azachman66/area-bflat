129bf7bdd0f3279c29275aa19caa0c23a734bbe2
Aspen Zachman <azachman66@gmail.com>
2026-03-22 22:00:37 -0400

    updating nav bar

--- public/js/comic_navBar.js
+++ public/js/comic_navBar.js
@@ -2,6 +2,7 @@ document.querySelector(".writeNavBar").innerHTML = `
 <div id="navbar">
 <div class="d-grid gap-2 d-md-block">
   <button><a href="../pages/comicMain.html">HOME</a></button>
+  <button><a href="../pages/comic.html?pg=1">COMIC</a></button>
   <button><a href="../pages/about.html">ABOUT</a></button>
   <button><a href="../pages/characters.html">CHARACTERS</a></button>
   <button><a href="../pages/archive.html">ARCHIVE</a></button>
